d1ce67611940daf1c66758b89bd64de376ebccf9
brennoc <64281238+brennoc@users.noreply.github.com>
2020-05-29 14:20:12 -0300

    Update Jogo.java

--- src/Jogo.java
+++ src/Jogo.java
@@ -3,21 +3,22 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int retornoMenu = menu();
 		if (retornoMenu == 1) {
 			historia();
 		}
-		
+
 		sc.close();
 	}
-	
-	static int menu(){
+
+	static int menu() { // função criada para armazenar o menu principal do jogo.
 		Scanner sc = new Scanner(System.in);
-		
-			int opcaoDigitada = 0;
-			
-			while (opcaoDigitada != 4) {
+
+		int opcaoDigitada = 0;
+
+		while ((opcaoDigitada != 4) && (opcaoDigitada != 1)) { // laço de repetição usado para repetir o menu enquanto
+																// não escolher as opções "jogar" e "sair".
 			System.out.println("*********************");
 			System.out.println("********MENU*********");
 			System.out.println("*********************");
@@ -25,44 +26,38 @@ public class Jogo {
 			System.out.println("*   2 - Pontuação   *");
 			System.out.println("*   3 - Creditos    *");
 			System.out.println("*   4 - Sair        *");
-			System.out.println("*********************");
-			System.out.println("");
+			System.out.println("*********************\n");
 			System.out.print("Digite a opção que deseja: ");
-				opcaoDigitada = sc.nextInt();
-			
+			opcaoDigitada = sc.nextInt();
+
 			System.out.println("");
-			
-				switch (opcaoDigitada) {
-					case 1: System.out.println("Entrando no jogo...");
-						break;
-					case 2: System.out.println("Mostrando sua pontuação...");
-						break;
-					case 3: System.out.println("Indo para os créditos...");
-							System.out.println("Projeto criado para a matéria de 'Projeto Integrador I - Professor: Eduardo Takeo.'");
-							System.out.println("O intuito dele é unir todas as materias do curso Tecnologia em Analíse e Desenvolvimento de Sistemas - Periodo I");
-							System.out.println("Desde já agradecemos pela preferencia e desejamos uma otima expêriencia.");
-							System.out.println("Desenvolvedores: Brenno C. Messias, Deivid C. Gomes e Juan G. Dias.");
-							System.out.println("");
-						break;
-					case 4: System.out.println("Saindo do jogo");
-						break;
-				
-					default: System.out.println("Opção invalida!!!");
-						break;
-					
-					}
-				
-						if (opcaoDigitada == 1) {
-							break;
-						}
-							System.out.println("");
-					} 
-		
+
+			switch (opcaoDigitada) {
+			case 1:
+				System.out.println("Entrando no jogo...\n");
+				break;
+			case 2:
+				System.out.println("Mostrando sua pontuação...\n");
+				break;
+			case 3:
+				System.out.printf(
+						"Indo para os créditos...\n\nProjeto criado para a matéria de 'Projeto Integrador I - Professor: Eduardo Takeo.'\nO intuito dele é unir todas as materias do curso Tecnologia em Analíse e Desenvolvimento de Sistemas - Periodo I\nDesde já agradecemos pela preferencia e desejamos uma otima expêriencia.\nDesenvolvedores: Brenno C. Messias, Deivid C. Gomes e Juan G. Dias.\n\n");
+				break;
+			case 4:
+				System.out.println("Saindo do jogo");
+				break;
+
+			default:
+				System.out.println("Opção invalida!!!\n");
+				break;
+			}
+		}
+
 		sc.close();
-				return opcaoDigitada;
+		return opcaoDigitada;
 	}
-	
-	static void historia(){
+
+	static void historia() {
 		System.out.println("***Colocar historia***");
 	}
 }
